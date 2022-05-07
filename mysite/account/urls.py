ddc74a318e7853debd73c82b9d278ce8e18695ab
sephiroth <sephiroth@gmail.com>
2022-05-07 23:29:24 +0800

    1.注意 request.POST 只能读取表单的数据 不能读取json参数 2.读取json参数需要用request.body 读取

--- mysite/account/urls.py
+++ mysite/account/urls.py
@@ -3,5 +3,5 @@ from .views import accounts
 
 app_name = 'account'
 urlpatterns = [
-    path('index/<str:search_key>', accounts, name='index'), # 查询账户
+    path('index', accounts, name='index'), # 查询账户
 ]
--- mysite/account/views.py
+++ mysite/account/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 from .models import MyAccount
 from django.db.models import Q
 
 
 # Create your views here.
-def accounts(request: HttpRequest, search_key=None):
+@csrf_exempt
+def accounts(request: HttpRequest):
     """
     查询账户信息
+    1. get方式获取所有的 账户信息
+    2. post方式 查询
     """
-    print("参数", search_key)
-    if search_key is None:
+    # 请求方式
+    method = request.method
+    print("请求方式",method)
+    if method == 'GET':
         """
             查询所有账户
         """
@@ -29,52 +36,39 @@ def accounts(request: HttpRequest, search_key=None):
                 'data': None,
                 'msg': '没有数据'
             })
-    else:
+    elif method == 'POST':
+        # 注意 request.POST 只能读取表单的数据 不能读取json参数
+        body = json.loads(request.body)
+        print(body)
+        search_key = body.get('search_key')
+
+
         """
         查询条件不为空 以描述和账户名作为查询 选项
         """
-        ac = MyAccount.objects.filter(
-            Q(account_description__contains=search_key) | Q(account_name__contains=search_key)).values()
-        count = ac.count()
-        if count > 0:
-            return JsonResponse({
-                'status': 200,
-                'count': count,
-                'data': list(ac),
-                'msg': ''
-            }, safe=False, )
+        if search_key is not None:
+            ac = MyAccount.objects.filter(
+                Q(account_description__contains=search_key) | Q(account_name__contains=search_key)).values()
+            count = ac.count()
+            if count > 0:
+                return JsonResponse({
+                    'status': 200,
+                    'count': count,
+                    'data': list(ac),
+                    'msg': ''
+                }, safe=False, )
+            else:
+                return JsonResponse({
+                    'status': 200,
+                    'count': count,
+                    'data': None,
+                    'msg': '没有数据'
+                })
+
         else:
             return JsonResponse({
                 'status': 200,
-                'count': count,
+                "flag":"参数为空",
                 'data': None,
                 'msg': '没有数据'
             })
-
-        # ac = MyAccount.objects.filter(account_description__contains=search_key).values()
-        # ac_number = ac.count()
-        # if ac_number > 0:
-        #     # print("账户描述",list(ac))
-        #     return JsonResponse({
-        #         'status': 200,
-        #         'data': list(ac),
-        #         'msg': ''
-        #     }, safe=False, )
-        # else:
-        #     ac = MyAccount.objects.filter(account_name__contains=search_key).values()
-        #     count = ac.count()
-        #     if count > 0:
-        #         print("账户名")
-        #         return JsonResponse({
-        #             'status': 200,
-        #             'count':count,
-        #             'data': list(ac),
-        #             'msg': ''
-        #
-        #         }, safe=False, )
-        #
-        # return JsonResponse({
-        #     'status': 200,
-        #         'data': None,
-        #         'msg': '没有数据'
-        # })
