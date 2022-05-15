7fc57427aaeb84fe5734a7621d1d34c4ac7ab31b
Sebastian-github-2020 <zaksvisitor@gmail.com>
2022-05-15 21:25:52 +0800

    Update run.py
    添加启动服务器命令 快捷方式

--- mysite/run.py
+++ mysite/run.py
@@ -1,10 +1,25 @@
-def data(status, data, **kwargs):
-    return {
-        "status": status,
-        "data": data,
-        **kwargs
-    }
+"""
+执行命令
+"""
+import os
+
+"""
+1. 启动服务器 manage.py
+"""
+default_port = "8080"
+
+base_command = "manage.py"
+
+run_server_default = f"{base_command} runserver"
+
+run_server_allow_other = f"{run_server_default} 0:0:0:0:{default_port}"
 
 
-a = data(200, 123, msg="msg")
-print(a)
+def run_server(command: str = base_command):
+    res = os.popen(command)
+    f = res.read()
+    print(f)
+
+
+if __name__ == '__main__':
+    run_server()
