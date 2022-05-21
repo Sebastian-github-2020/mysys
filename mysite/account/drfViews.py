8598668fdf79e4a5c4b5ed47fd7496ac2a939d27
Sebastian-github-2020 <zaksvisitor@gmail.com>
2022-05-21 18:19:54 +0800

    1. 针对一条数据的api操作

--- mysite/account/drfViews.py
+++ mysite/account/drfViews.py
@@ -33,13 +33,59 @@ class MyAccountAPIView(APIView):
         print("post", data)
         # 將传入的数据 存入数据库，这属于反序列化  需要验证格式
         serializer = AccountSerializer(data=data)
-        serializer.save()  # 很重要 切记
+
         if serializer.is_valid():
+            serializer.save()  # 很重要 切记
             return Response(data=data)
         else:
             return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class MyAccountInfoAPIView(APIView):
+    def get(self, request: Request, account_id: int):
+        """
+        获取一条用户信息
+        :param request:
+        :param account_id:
+        :return:
+        """
+        try:
+            account = MyAccount.objects.get(id=account_id)
+        except MyAccount.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        # 序列化
+        serializer = AccountSerializer(instance=account)
+
+        # 返回结果
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request: Request, account_id: int):
+        """
+        更新数据
+        :param request:
+        :param account_id: 账户id
+        :return:
+        """
+        # 获取客户端提交的数据
+        data = request.data
+        # 查询需要更新的数据
+        account = MyAccount.objects.get(id=account_id)
+        # 反序列化
+        serializer = AccountSerializer(instance=account, data=data)
+        # 验证
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request: Request, account_id):
+        """删除数据"""
+        pass
+
+
 class MyAccountView(ModelViewSet):
+    """这种做法不常用,不能满足需求"""
     queryset = MyAccount.objects.all()
     serializer_class = AccountSerializer
--- mysite/account/urls.py
+++ mysite/account/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from .views import accounts
-from .drfViews import MyAccountView, MyAccountAPIView
+from .drfViews import MyAccountView, MyAccountAPIView,MyAccountInfoAPIView
 from rest_framework.routers import DefaultRouter
+# from rest_framework.urls import
 
 router = DefaultRouter()
 router.register("api", MyAccountView)
 app_name = 'account'
 urlpatterns = [
                   path('index', accounts, name='index'),  # 查询账户
-                  path("drfapi", MyAccountAPIView.as_view())
+                  path("drfapi", MyAccountAPIView.as_view()),
+                  path("drfapi1/<int:account_id>", MyAccountInfoAPIView.as_view()),
               ] + router.urls
